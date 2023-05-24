ff736ae17de79a191fa410c32aae98a984fd6935
Wilson Santos <contato@wilsonsantos.net>
2023-05-24 07:55:01 -0300

    Add Categories Validation

--- ViewModels/EditorCategoryViewModel.cs
+++ ViewModels/EditorCategoryViewModel.cs
@@ -1,8 +1,13 @@
-﻿namespace Blog.ViewModels
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Blog.ViewModels
 {
     public class EditorCategoryViewModel
     {
+        [Required(ErrorMessage = "O nome é obrigatório.")]
+        [StringLength(80, MinimumLength =3, ErrorMessage = "O nome deve ter entre 3 e 80 caracteres")]
         public string Name { get; set; }
+        [Required(ErrorMessage = "O slug é obrigatório.")]
         public string Slug { get; set; }
     }
 }
