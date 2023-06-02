722495edef4bf4f9091bfc469e72a8b6f397fa59
Wilson Santos <contato@wilsonsantos.net>
2023-06-02 17:29:14 -0300

    Melhora o retorno de erros

--- Controllers/CategoryCrontroller.cs
+++ Controllers/CategoryCrontroller.cs
@@ -1,4 +1,5 @@
 ﻿using Blog.Data;
+using Blog.Extensions;
 using Blog.Models;
 using Blog.ViewModels;
 using Microsoft.AspNetCore.Mvc;
@@ -13,20 +14,36 @@ namespace Blog.Controllers
         [HttpGet("v1/categories")]
         public async Task <IActionResult> GetAsync(BlogDataContext context)
         {
-            var categories = await context.Categories.ToListAsync();
-            return Ok(categories);
+            try
+            {
+                var categories = 
+                    await context.Categories.ToListAsync();
+                return Ok(new ResultViewModel<List<Category>>(categories));
+            }
+            catch
+            {
+                return StatusCode(500, new ResultViewModel<List<Category>>("Falha interna do servidor"));
+            }
         }
 
         [HttpGet("v1/categories/{id:int}")]
         
         public async Task<IActionResult> GetByIdAsync([FromRoute] int id, BlogDataContext context)
         {
-            Category? categories = await GetById(id, context);
+            try
+            {
+                Category? categories = await GetById(id, context);
 
-            if (categories == null)
-                return NotFound();
+                if (categories == null)
+                    return NotFound();
 
-            return Ok(categories);
+                return Ok(new ResultViewModel<Category>(categories));
+            }
+            catch
+            {
+                return StatusCode(500, new ResultViewModel<Category>("Erro interno do servidor"));
+            }
+            
         }
 
         
@@ -36,15 +53,30 @@ namespace Blog.Controllers
             [FromBody] EditorCategoryViewModel model,
             BlogDataContext context)
         {
+            if (!ModelState.IsValid)
+                return BadRequest(new ResultViewModel<Category>(ModelState.GetErrors()));
+
             var category = new Category
             {
                 Name = model.Name,
                 Slug = model.Slug
             };
 
-            await context.AddAsync(category);
-            await context.SaveChangesAsync();
-            return Created($"v1/categories/{category.Id}",category);
+            try
+            {
+                await context.AddAsync(category);
+                await context.SaveChangesAsync();
+                return Created($"v1/categories/{category.Id}", new ResultViewModel<Category>(category));
+            }
+            catch (DbUpdateException e)
+            {
+                return StatusCode(500, new ResultViewModel<Category>($"Falha ao adicionar a categoria\n{e.Message} - {e.InnerException}"));
+            }
+            catch
+            {
+                return StatusCode(500, new ResultViewModel<Category>("Falha interna do servidor"));
+            }
+            
         }
 
         [HttpPut("v1/categories/{id:int}")]
@@ -54,6 +86,9 @@ namespace Blog.Controllers
             [FromBody] EditorCategoryViewModel model,
             BlogDataContext context)
         {
+            if (!ModelState.IsValid)
+                return BadRequest(new ResultViewModel<Category>(ModelState.GetErrors()));
+
             Category? category = await GetById(id, context);
 
             if (category == null)
@@ -61,10 +96,24 @@ namespace Blog.Controllers
 
             category.Name = model.Name;
             category.Slug = model.Slug;
-            context.Update(category);
-            await context.SaveChangesAsync();
 
-            return Ok(model);
+            try
+            {
+                context.Update(category);
+                await context.SaveChangesAsync();
+
+                return Ok(new ResultViewModel<Category>(category));
+            }
+            catch (DbUpdateException e)
+            {
+                return StatusCode(500, new ResultViewModel<Category>($"Falha ao atualizar a categoria\n{e.Message} - {e.InnerException}"));
+            }
+            catch
+            {
+                return StatusCode(500, new ResultViewModel<Category>("Falha interna do servidor"));
+            }
+
+            
         }
 
         [HttpDelete("v1/categories/{id:int}")]
@@ -74,11 +123,22 @@ namespace Blog.Controllers
             Category? category = await GetById(id, context);
             if (category == null)
                 return NotFound();
+            try
+            {
+                context.Remove(category);
+                await context.SaveChangesAsync();
 
-            context.Remove(category);
-            await context.SaveChangesAsync();
-
-            return Ok(category);
+                return Ok(category);
+            }
+            catch (DbUpdateException e)
+            {
+                return StatusCode(500, new ResultViewModel<Category>($"Falha ao remover a categoria\n{e.Message} - {e.InnerException}"));
+            }
+            catch
+            {
+                return StatusCode(500, new ResultViewModel<Category>("Falha interna do servidor"));
+            }
+            
         }
 
         private static async Task<Category?> GetById(int id, BlogDataContext context)
---
+++ Extensions/ModelStateExtension.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding;
+
+namespace Blog.Extensions
+{
+    public static class ModelStateExtension
+    {
+        public static List<string> GetErrors(this ModelStateDictionary modelState)
+        {
+            var errors = new List<string>();
+            foreach (var item in modelState)
+            {
+                var erros = item.Value.Errors.Select(e => e.ErrorMessage);
+                errors.AddRange(erros);
+            }
+            return errors;
+        }
+    }
+}
