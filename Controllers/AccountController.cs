27ec9fdbcecd06634f4020d23816812a69d01b05
Wilson Santos <contato@wilsonsantos.net>
2023-06-03 14:17:13 -0300

    feat:Add Auth

---
+++ Controllers/AccountController.cs
@@ -0,0 +1,86 @@
+﻿using Blog.Data;
+using Blog.Extensions;
+using Blog.Models;
+using Blog.Services;
+using Blog.ViewModels;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using SecureIdentity.Password;
+
+namespace Blog.Controllers
+{
+    [ApiController]
+    public class AccountController : ControllerBase
+    {
+        [HttpPost("v1/account/login")]
+        public async Task<IActionResult> Post(
+            [FromBody] LoginViewModel model,
+            [FromServices] BlogDataContext context,
+            [FromServices] TokenService tokenService)
+        {
+            if(!ModelState.IsValid)
+                return BadRequest(new ResultViewModel<string>(ModelState.GetErrors()));
+
+            var user = await context
+                .Users
+                .AsNoTracking()
+                .Include(x=>x.Roles)
+                .FirstOrDefaultAsync(x => x.Email == model.Email);
+            
+            if(user == null)
+                return StatusCode(401, new ResultViewModel<string>("Usuário ou senha inválidos"));
+            if(!PasswordHasher.Verify(user.PasswordHash,model.Password))
+                return StatusCode(401, new ResultViewModel<string>("Usuário ou senha inválidos"));
+
+            try
+            {
+                var token = tokenService.GenerateToken(user);
+                return Ok(new ResultViewModel<string>(token, null));
+            }
+            catch
+            {
+                return StatusCode(500, new ResultViewModel<string>("Falha interna do servidor"));
+            }
+
+        }
+
+        [HttpPost("v1/account/")]
+        public async Task<IActionResult> Post(
+            [FromBody] RegisterViewModel model,
+            [FromServices] BlogDataContext context)
+        {
+            if(!ModelState.IsValid)
+                return BadRequest(new ResultViewModel<User>(ModelState.GetErrors()));
+
+            var user = new User
+            {
+                Name = model.Name,
+                Email = model.Email,
+                Slug = model.Email.Replace("@", "-").Replace(".", "-")
+            };
+
+            var password = PasswordGenerator.Generate(25);
+            user.PasswordHash = PasswordHasher.Hash(password);
+
+            try
+            {
+                await context.Users.AddAsync(user);
+                await context.SaveChangesAsync();
+
+                return Ok(new ResultViewModel<dynamic>(new
+                {
+                    user = user.Email, password
+                }));
+            }
+            catch (DbUpdateException e)
+            {
+                return StatusCode(400, new ResultViewModel<string>(e.InnerException?.Message ?? e.Message));
+            }
+            catch
+            {
+                return StatusCode(500, new ResultViewModel<string>("Falha interna do servidor"));
+            }
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,6 +1,28 @@
+using System.Text;
+using Blog;
 using Blog.Data;
+using Blog.Services;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
 
 var builder = WebApplication.CreateBuilder(args);
+
+var key = Encoding.ASCII.GetBytes(Configuration.JwtKey);
+
+builder.Services.AddAuthentication(x =>
+{
+    x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    x.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+}).AddJwtBearer(x =>
+{
+    x.TokenValidationParameters = new TokenValidationParameters()
+    {
+        ValidateIssuerSigningKey = true,
+        IssuerSigningKey = new SymmetricSecurityKey(key),
+        ValidateIssuer = false,
+        ValidateAudience = false
+    };
+});
 
 builder.Services
     .AddControllers()
@@ -8,9 +30,14 @@ builder.Services
 {
     options.SuppressModelStateInvalidFilter = true;
 });
+
 builder.Services.AddDbContext<BlogDataContext>();
+builder.Services.AddTransient<TokenService>();
 
 var app = builder.Build();
+
+app.UseAuthentication();
+app.UseAuthorization();
 
 app.MapControllers();
 
